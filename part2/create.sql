f40f6a5fa69040d7c68626dafe1b3cfaea0ed3f1
Étienne B. Auger <etienne.bauger@gmail.com>
2018-04-27 16:30:36 -0400

    Part 2 version 2

--- part2/create.sql
+++ part2/create.sql
@@ -1,118 +1,52 @@
-create table Sources
+USE udem_part2;
+create table html_version
 (
-  id   int auto_increment
+  id          int auto_increment
     primary key,
-  html text null,
-  constraint Source_id_uindex
-  unique (id)
-)
-  comment 'History of the HTML page'
-  engine = InnoDB;
+  page_url_id int,
+  html        text null
+);
 
-create table States
+create table page_status
 (
-  id                int auto_increment
+  id               int auto_increment
     primary key,
-  state             varchar(20) not null,
-  last_verification datetime    null,
-  firstime          datetime    null,
-  last_changes      datetime    null,
-  number_versions   int         null,
-  constraint States_id_uindex
-  unique (id),
-  constraint States_state_uindex
-  unique (state)
-)
-  engine = InnoDB;
+  description_name varchar(20)
+);
 
-create table Tags
+create table tag
 (
   id      int auto_increment
     primary key,
   name_fr varchar(20) not null,
-  name_en varchar(20) null,
-  constraint Tags_id_uindex
-  unique (id),
-  constraint Tags_name_fr_uindex
-  unique (name_fr),
-  constraint Tags_name_en_uindex
-  unique (name_en)
-)
-  engine = InnoDB;
+  name_en varchar(20) not null
 
-create table Websites
+);
+
+create table tag_website_page_url
 (
-  id   int auto_increment
+  id         int auto_increment primary key,
+  tag_id     int not null,
+  page_id    int null,
+  website_id int null
+);
+
+create table website
+(
+  id       int auto_increment
     primary key,
-  name varchar(20) null,
-  url  text        null,
-  constraint Websites_id_uindex
-  unique (id)
-)
-  engine = InnoDB;
+  name     varchar(20)   null,
+  root_url varchar(2083) null
+);
 
-create table Pages
+create table page_url
 (
-  id          int auto_increment
+  id         int auto_increment
     primary key,
-  url         varchar(2083) not null,
-  last_time   int           null,
-  first_time  int           null,
-  last_access int           null,
-  website_id  int           null,
-  state_id    int           null,
-  source_id   int           null,
-  constraint Pages_id_uindex
-  unique (id),
-  constraint Pages_Websites_id_fk
-  foreign key (website_id) references Websites (id),
-  constraint Pages_States_id_fk
-  foreign key (state_id) references States (id),
-  constraint Pages_Sources_id_fk
-  foreign key (source_id) references Sources (id)
-)
-  engine = InnoDB;
-
-create index Pages_Sources_id_fk
-  on Pages (source_id);
-
-create index Pages_States_id_fk
-  on Pages (state_id);
-
-create index Pages_Websites_id_fk
-  on Pages (website_id);
-
-create table Tags_Pages
-(
-  page_id int not null,
-  tag_id  int not null,
-  constraint Tags_Pages_page_id_uindex
-  unique (page_id),
-  constraint Tags_Pages_tag_id_uindex
-  unique (tag_id),
-  constraint Tags_Pages_Pages_id_fk
-  foreign key (page_id) references Pages (id),
-  constraint Tags_Pages_Websites_id_fk
-  foreign key (page_id) references Websites (id),
-  constraint Tags_Pages_Tags_id_fk
-  foreign key (tag_id) references Tags (id)
-)
-  engine = InnoDB;
-
-create table Tags_Websites
-(
-  website_id int null,
-  tag_id     int null,
-  constraint Tags_Websites_Websites_id_fk
-  foreign key (website_id) references Websites (id),
-  constraint Tags_Websites_Tags_id_fk
-  foreign key (tag_id) references Tags (id)
-)
-  engine = InnoDB;
-
-create index Tags_Websites_Tags_id_fk
-  on Tags_Websites (tag_id);
-
-create index Tags_Websites_Websites_id_fk
-  on Tags_Websites (website_id);
+  website_id int,
+  url_path   varchar(2083) not null,
+  last_check int           null,
+  first_time int           null,
+  last_time  int           null
+);
 
--- part2/req.sql
+++ part2/req.sql
@@ -1,29 +1,72 @@
+
 USE udem_part2;
-#requete1
+
+/*
+requete1
+Tous les sites incluant le nombre de pages de chacun;
+ */
+-- DISTINCT I presume each website are unique
 SELECT
-  w.name
-  count(p.website_id)
+  w.name,
+  count(pu.website_id) AS page_count
+FROM website AS w
+LEFT JOIN page_url AS pu
+  ON w.id = pu.website_id;
+
+/*
+requete2 
+Toutes les étiquettes incluant le nombre de sites 
+et de pages associées à chacune;*/
+
+-- I presume each tag have minimum association with a page or website; if not, no association on this table.
+SELECT
+  t.name_fr, t.name_en,
+  count(w.id) as website_count,
+  count(pu.id) as page_count
 FROM
-  Pages as p, Websites as w
-LEFT JOIN
-  Pages ON w.id = p.website_id;
+  tag_website_page_url AS twpu
+LEFT JOIN tag as t
+  ON twpu.tag_id = t.id
+LEFT JOIN website AS w
+  ON twpu.page_id = p.id
+LEFT JOIN page_url AS pu
+  ON twpu.website_id = w.id;
+/*
+requete3 
+Toutes les pages d'un site incluant le nombre de versions de contenu disponibles
+et la taille totale de toutes les versions de chacune;
+*/
+SELECT
+  pu.url_path,
+  count(hv.id),
+  LENGTH(hv.html) as size_kb -- 
+FROM page_url as pu
+LEFT JOIN html_version as hv
+  ON pu.id = hv.page_url_id
+WHERE pu.id IN (
+  SELECT DISTINCT id
+  FROM website
+  WHERE root_url = @search
+);
 
-#requete2
-FROM
-  Tags as t
-  Websites as w
-  Pages as p
-
-#requete3
-
-#requete4 todo create procedure
+/*
+requete4
+Combien de pages d'un site donné dont l'URL inclut ​"/products/"​ 
+ont du contenu plus récent que le moment X 
+et un état spécifique.
+*/
 SELECT
   COUNT(id)
-FROM
-  Pages as p,
-  States as s
-WHERE
-  (p.url LIKE '%/products/%') AND
-  (TIMEDIFF(s.last_verification, 'date')) AND
-  (s.state = 'stateName')
-;
+FROM page_url as pu
+LEFT JOIN page_status as ps
+  ON pu.id IN (
+    SELECT id
+    FROM page_status
+    WHERE page_status.description_name = @state_name
+  )
+WHERE pu.id IN (
+  SELECT DISTINCT id
+  FROM website
+  WHERE root_url = @exact_url
+) AND ((pu.url_path LIKE '%/products/%'))
+  AND (TIMEDIFF(pu.last_check, @exact_date))
